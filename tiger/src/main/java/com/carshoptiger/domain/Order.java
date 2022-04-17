bb72bc8f4ad9b4f2b2e9c7101994767205cd234a
Andrew Ognew <han2332andrew@gmail.com>
2022-04-17 19:38:28 +0300

    Add domain for order

---
+++ tiger/src/main/java/com/carshoptiger/domain/Order.java
@@ -0,0 +1,25 @@
+package com.carshoptiger.domain;
+
+import lombok.*;
+
+import java.sql.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
+public class Order {
+    private Long id;
+    private String name;
+    private String soname;
+    private String faname;
+    private String priceorder;
+    private String country;
+    private String city;
+    private String address;
+    private String contactphone;
+    private String commentorder;
+    private Date date_order;
+}
