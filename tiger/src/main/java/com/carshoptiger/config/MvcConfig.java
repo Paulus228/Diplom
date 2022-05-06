9d694ee1c8a42a80a391b945043cd56bd52677eb
Andrew Ognew <han2332andrew@gmail.com>
2022-05-06 22:13:56 +0300

    Add mvc config and fix admin template

---
+++ tiger/src/main/java/com/carshoptiger/config/MvcConfig.java
@@ -0,0 +1,19 @@
+package com.carshoptiger.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
+    }
+
+//    @Override
+//    public void addViewControllers(ViewControllerRegistry registry) {
+//        registry.addViewController("/login").setViewName("login");
+//    }
+}
