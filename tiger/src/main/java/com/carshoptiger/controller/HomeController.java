3414c99b1fee03fe735b75d28d127854d4284f4e
Andrew Ognew <han2332andrew@gmail.com>
2022-05-06 23:23:51 +0300

    Testimonals block in home page

--- tiger/src/main/java/com/carshoptiger/controller/HomeController.java
+++ tiger/src/main/java/com/carshoptiger/controller/HomeController.java
@@ -1,9 +1,11 @@
 package com.carshoptiger.controller;
 
 import com.carshoptiger.domain.Car;
+import com.carshoptiger.domain.Testimonals;
 import com.carshoptiger.service.API.CarInfoService;
 import com.carshoptiger.service.API.CarPhotoService;
 import com.carshoptiger.service.API.CarService;
+import com.carshoptiger.service.API.TestimonalsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -24,14 +26,19 @@ public class HomeController {
     @Autowired
     private CarInfoService carInfoService;
 
+    @Autowired
+    private TestimonalsService testimonalsService;
+
     @GetMapping("/")
     public String homePage(Model model){
         List<Car> carList = carService.findAllCar().stream().limit(6).collect(Collectors.toList());
+        List<Testimonals> testimonalsList = testimonalsService.findAllTestimonals().stream().limit(6).collect(Collectors.toList());
 
         model.addAttribute("page","Home");
         model.addAttribute("carlist",carList);
         model.addAttribute("carphotoservice",carPhotoService);
         model.addAttribute("carinfoservice",carInfoService);
+        model.addAttribute("testimonals",testimonalsList);
         return "user/index";
     }
 
