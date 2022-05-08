2ac421a1a1a066a6ada4752efeb9dfd2cb7bdd89
Andrew Ognew <han2332andrew@gmail.com>
2022-05-08 18:38:59 +0300

    Car list page

---
+++ tiger/src/main/java/com/carshoptiger/controller/CarsController.java
@@ -0,0 +1,34 @@
+package com.carshoptiger.controller;
+
+import com.carshoptiger.service.API.CarInfoService;
+import com.carshoptiger.service.API.CarPhotoService;
+import com.carshoptiger.service.API.CarService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/cars")
+public class CarsController {
+
+    @Autowired
+    private CarService carService;
+
+    @Autowired
+    private CarPhotoService carPhotoService;
+
+    @Autowired
+    private CarInfoService carInfoService;
+
+
+    @GetMapping("/all")
+    public String carlist(Model model){
+        model.addAttribute("carlist",carService.findAllCar());
+        model.addAttribute("carphotoservice",carPhotoService);
+        model.addAttribute("carinfoservice",carInfoService);
+        return "user/carlist";
+    }
+
+}
