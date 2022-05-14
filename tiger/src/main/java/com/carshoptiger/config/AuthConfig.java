59459a2814fa50405ef83b95c7ca93637d2c6dcc
Andrew Ognew <han2332andrew@gmail.com>
2022-05-14 17:11:45 +0300

    Auth config

---
+++ tiger/src/main/java/com/carshoptiger/config/AuthConfig.java
@@ -0,0 +1,31 @@
+package com.carshoptiger.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class AuthConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/cars/**","/contact/**","/static/**","/**",
+                "/testimonals","/registration").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/")
+                .permitAll()
+                .and()
+                .logout()
+                .logoutSuccessUrl("/")
+                .permitAll();
+    }
+
+
+}
